aa59ddef1a9a83a5da0e3d3b8e95394cbe8c8e40
manfromexistence <89142466+manfromexistence@users.noreply.github.com>
2025-02-01 12:39:45 +0000

    feat: add LogoIcon component and update TeamSwitcher layout

--- apps/www/registry/new-york/blocks/sidebar-07/components/team-switcher.tsx
+++ apps/www/registry/new-york/blocks/sidebar-07/components/team-switcher.tsx
@@ -20,6 +20,10 @@ import {
 } from "@/registry/new-york/ui/sidebar"
 import { Avatar, AvatarFallback, AvatarImage } from "@/registry/new-york/ui/avatar"
 
+import type { SVGProps } from "react";
+const LogoIcon = (props: SVGProps<SVGSVGElement>) => <svg height="1em" xmlns="http://www.w3.org/2000/svg" strokeLinejoin="round" viewBox="0 0 16 16" width="1em" {...props}><path clipRule="evenodd" d="M9.50321 5.5H13.2532C13.3123 5.5 13.3704 5.5041 13.4273 5.51203L9.51242 9.42692C9.50424 9.36912 9.5 9.31006 9.5 9.25L9.5 5.5L8 5.5L8 9.25C8 10.7688 9.23122 12 10.75 12H14.5V10.5L10.75 10.5C10.6899 10.5 10.6309 10.4958 10.5731 10.4876L14.4904 6.57028C14.4988 6.62897 14.5032 6.68897 14.5032 6.75V10.5H16.0032V6.75C16.0032 5.23122 14.772 4 13.2532 4H9.50321V5.5ZM0 5V5.00405L5.12525 11.5307C5.74119 12.3151 7.00106 11.8795 7.00106 10.8822V5H5.50106V9.58056L1.90404 5H0Z" fill="white" fillRule="evenodd" /></svg>;
+export default LogoIcon;
+
 export function TeamSwitcher({
   teams,
 }: {
@@ -41,14 +45,16 @@ export function TeamSwitcher({
               size="lg"
               className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
             >
-              <Avatar className="h-8 w-8 rounded-lg">
+              {/* <Avatar className="h-8 w-8 rounded-lg">
                 <AvatarImage src={"/avatars/shadcn.jpg"} alt={"shadcn"} />
                 <AvatarFallback className="rounded-lg">CN</AvatarFallback>
-              </Avatar>
+              </Avatar> */}
 
-              {/* <div className="flex aspect-square size-8 items-center justify-center rounded-lg bg-sidebar-primary bg-yellow-500 text-sidebar-primary-foreground data-[state=open]:bg-green-500">
-                <activeTeam.logo className="size-4" />
-              </div> */}
+              <div className="flex aspect-square size-8 items-center justify-center rounded-lg bg-yellow-500 text-sidebar-primary-foreground data-[state=open]:bg-green-500">
+                {/* <activeTeam.logo className="size-4" /> */}
+              <LogoIcon />
+
+              </div>
               <div className="grid flex-1 text-left text-sm leading-tight">
                 <span className="truncate font-semibold">
                   {activeTeam.name}
