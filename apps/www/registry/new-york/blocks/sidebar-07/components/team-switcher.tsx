1ed4a315c8083ac21e0f76862458d1388a006062
manfromexistence <ajju40959@gmail.com>
2025-02-01 02:39:52 +0000

    feat: sidebar07 was logo problem

--- apps/www/registry/new-york/blocks/sidebar-07/components/team-switcher.tsx
+++ apps/www/registry/new-york/blocks/sidebar-07/components/team-switcher.tsx
@@ -18,6 +18,7 @@ import {
   SidebarMenuItem,
   useSidebar,
 } from "@/registry/new-york/ui/sidebar"
+import { Avatar, AvatarFallback, AvatarImage } from "@/registry/new-york/ui/avatar"
 
 export function TeamSwitcher({
   teams,
@@ -40,9 +41,14 @@ export function TeamSwitcher({
               size="lg"
               className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
             >
-              <div className="flex aspect-square size-8 items-center justify-center rounded-lg bg-sidebar-primary bg-yellow-500 text-sidebar-primary-foreground data-[state=open]:bg-green-500">
+              <Avatar className="h-8 w-8 rounded-lg">
+                <AvatarImage src={"/avatars/shadcn.jpg"} alt={"shadcn"} />
+                <AvatarFallback className="rounded-lg">CN</AvatarFallback>
+              </Avatar>
+
+              {/* <div className="flex aspect-square size-8 items-center justify-center rounded-lg bg-sidebar-primary bg-yellow-500 text-sidebar-primary-foreground data-[state=open]:bg-green-500">
                 <activeTeam.logo className="size-4" />
-              </div>
+              </div> */}
               <div className="grid flex-1 text-left text-sm leading-tight">
                 <span className="truncate font-semibold">
                   {activeTeam.name}
