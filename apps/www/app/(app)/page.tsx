2ac83d284434b3fed2ba5b9dc13d2809b2fe6737
manfromexistence <89142466+manfromexistence@users.noreply.github.com>
2025-02-02 16:54:33 +0000

    feat: update sidebar styles and adjust background colors

--- apps/www/app/(app)/page.tsx
+++ apps/www/app/(app)/page.tsx
@@ -32,10 +32,10 @@ export default function Page() {
           <NavActions />
         </div>
       </header>
-      <div className="flex flex-1 flex-col gap-4 px-4 py-10">
+      {/* <div className="flex flex-1 flex-col gap-4 px-4 py-10">
         <div className="mx-auto h-24 w-full max-w-3xl rounded-xl bg-muted/50" />
         <div className="mx-auto h-full w-full max-w-3xl rounded-xl bg-muted/50" />
-      </div>
+      </div> */}
     </SidebarInset>
   )
 }
