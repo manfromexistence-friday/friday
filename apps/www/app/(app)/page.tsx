198905a00c61a146072c44ea5dd12f19cb30ccf0
manfromexistence <ajju40959@gmail.com>
2025-02-13 16:15:18 +0000

    feat: will fix right sidebar problem later

--- apps/www/app/(app)/page.tsx
+++ apps/www/app/(app)/page.tsx
@@ -9,36 +9,170 @@ import { Button } from "@/registry/new-york/ui/button"
 import { Separator } from "@/registry/new-york/ui/separator"
 import {
   SidebarInset,
-  SidebarTrigger,
+
 } from "@/registry/new-york/ui/sidebar"
 import { Settings, Lock } from "lucide-react"
+import {
+  Cloud,
+  CreditCard,
+  Github,
+  Keyboard,
+  LifeBuoy,
+  LogOut,
+  Mail,
+  MessageSquare,
+  Plus,
+  PlusCircle,
+  User,
+  UserPlus,
+  Users,
+} from "lucide-react"
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuPortal,
+  DropdownMenuSeparator,
+  DropdownMenuShortcut,
+  DropdownMenuSub,
+  DropdownMenuSubContent,
+  DropdownMenuSubTrigger,
+  DropdownMenuTrigger,
+} from "@/registry/new-york/ui/dropdown-menu"
+import { SidebarProvider, SidebarTrigger } from "@/registry/new-york/ui/sidebar"
+import { Sidebar, SidebarContent } from "@/registry/new-york/ui/sidebar"
+
+export function AppSidebar() {
+  return (
+    <Sidebar side="right">
+      <SidebarContent />
+    </Sidebar>
+  )
+}
+
 
 export default function Page() {
   return (
     <SidebarInset>
-      <header className="flex h-12 shrink-0 items-center gap-2">
-        <div className="flex flex-1 items-center gap-2 px-3">
-          <Breadcrumb>
-            <BreadcrumbList>
-              <BreadcrumbItem>
-                <BreadcrumbPage className="line-clamp-1">
-                  Project Management & Task Tracking
-                </BreadcrumbPage>
-                <div className="flex items-center justify-center gap-0.5 rounded-full border px-2 py-[2.5px] text-[0.60rem] text-primary">
-                  <Lock className="h-[13px] w-[13px]" />
-                  <span className="">
-                    Private
-                  </span>
-                </div>
-              </BreadcrumbItem>
-            </BreadcrumbList>
-          </Breadcrumb>
-        </div>
-        <div className="ml-auto px-3">
-          {/* <NavActions /> */}
-          
-        </div>
-      </header>
+      <SidebarProvider>
+        <AppSidebar />
+        <main>
+          <header className="flex h-12 shrink-0 items-center gap-2">
+            <div className="flex flex-1 items-center gap-2 px-3">
+              <Breadcrumb>
+                <BreadcrumbList>
+                  <BreadcrumbItem>
+                    <BreadcrumbPage className="line-clamp-1">
+                      Project Management & Task Tracking
+                    </BreadcrumbPage>
+                    <div className="flex items-center justify-center gap-0.5 rounded-full border px-2 py-[2.5px] text-[0.60rem] text-primary">
+                      <Lock className="h-[13px] w-[13px]" />
+                      <span className="h-full">
+                        Private
+                      </span>
+                    </div>
+                  </BreadcrumbItem>
+                </BreadcrumbList>
+              </Breadcrumb>
+            </div>
+            <div className="ml-auto px-3">
+              {/* <NavActions /> */}
+              <SidebarTrigger />
+
+              <DropdownMenu>
+                <DropdownMenuTrigger asChild>
+                  <Button variant="outline">Chatgpt</Button>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent className="w-56">
+                  <DropdownMenuLabel>My Account</DropdownMenuLabel>
+                  <DropdownMenuSeparator />
+                  <DropdownMenuGroup>
+                    <DropdownMenuItem>
+                      <User />
+                      <span>Profile</span>
+                      <DropdownMenuShortcut>⇧⌘P</DropdownMenuShortcut>
+                    </DropdownMenuItem>
+                    <DropdownMenuItem>
+                      <CreditCard />
+                      <span>Billing</span>
+                      <DropdownMenuShortcut>⌘B</DropdownMenuShortcut>
+                    </DropdownMenuItem>
+                    <DropdownMenuItem>
+                      <Settings />
+                      <span>Settings</span>
+                      <DropdownMenuShortcut>⌘S</DropdownMenuShortcut>
+                    </DropdownMenuItem>
+                    <DropdownMenuItem>
+                      <Keyboard />
+                      <span>Keyboard shortcuts</span>
+                      <DropdownMenuShortcut>⌘K</DropdownMenuShortcut>
+                    </DropdownMenuItem>
+                  </DropdownMenuGroup>
+                  <DropdownMenuSeparator />
+                  <DropdownMenuGroup>
+                    <DropdownMenuItem>
+                      <Users />
+                      <span>Team</span>
+                    </DropdownMenuItem>
+                    <DropdownMenuSub>
+                      <DropdownMenuSubTrigger>
+                        <UserPlus />
+                        <span>Invite users</span>
+                      </DropdownMenuSubTrigger>
+                      <DropdownMenuPortal>
+                        <DropdownMenuSubContent>
+                          <DropdownMenuItem>
+                            <Mail />
+                            <span>Email</span>
+                          </DropdownMenuItem>
+                          <DropdownMenuItem>
+                            <MessageSquare />
+                            <span>Message</span>
+                          </DropdownMenuItem>
+                          <DropdownMenuSeparator />
+                          <DropdownMenuItem>
+                            <PlusCircle />
+                            <span>More...</span>
+                          </DropdownMenuItem>
+                        </DropdownMenuSubContent>
+                      </DropdownMenuPortal>
+                    </DropdownMenuSub>
+                    <DropdownMenuItem>
+                      <Plus />
+                      <span>New Team</span>
+                      <DropdownMenuShortcut>⌘+T</DropdownMenuShortcut>
+                    </DropdownMenuItem>
+                  </DropdownMenuGroup>
+                  <DropdownMenuSeparator />
+                  <DropdownMenuItem>
+                    <Github />
+                    <span>GitHub</span>
+                  </DropdownMenuItem>
+                  <DropdownMenuItem>
+                    <LifeBuoy />
+                    <span>Support</span>
+                  </DropdownMenuItem>
+                  <DropdownMenuItem disabled>
+                    <Cloud />
+                    <span>API</span>
+                  </DropdownMenuItem>
+                  <DropdownMenuSeparator />
+                  <DropdownMenuItem>
+                    <LogOut />
+                    <span>Log out</span>
+                    <DropdownMenuShortcut>⇧⌘Q</DropdownMenuShortcut>
+                  </DropdownMenuItem>
+                </DropdownMenuContent>
+              </DropdownMenu>
+
+
+            </div>
+          </header>
+        </main>
+      </SidebarProvider>
+
     </SidebarInset>
   )
 }
