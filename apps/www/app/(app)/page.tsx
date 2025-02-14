41bbf0b8c48e2bdb7b040fef3a013ad6b2fa77d9
manfromexistence <ajju40959@gmail.com>
2025-02-14 15:03:53 +0000

    feat: image position

--- apps/www/app/(app)/page.tsx
+++ apps/www/app/(app)/page.tsx
@@ -1,7 +1,7 @@
 "use client"
 import { Button } from "@/registry/new-york/ui/button"
 import { Separator } from "@/registry/new-york/ui/separator"
-import { ChevronDown, Earth, Type, MessageCircle } from "lucide-react"
+import { ChevronDown, Earth, Type, MessageCircle, Microscope } from "lucide-react"
 import { CategorySidebarProvider } from "@/components/sidebar/category-sidebar"
 import { SubCategorySidebarProvider } from "@/components/sidebar/sub-category-sidebar"
 import { CategorySidebar, CategorySidebarContent } from "@/components/sidebar/category-sidebar"
@@ -28,6 +28,8 @@ import {
   Sparkles,
   SquareTerminal,
   Trash2,
+  CircleDotDashed
+
 } from "lucide-react"
 import { NavActions } from "@/components/sidebar/nav-actions"
 import { Tooltip } from 'antd';
@@ -65,6 +67,330 @@ import {
   useSubCategorySidebar,
 } from "@/components/sidebar/sub-category-sidebar"
 import { SidebarProvider } from "@/components/sidebar/actions-sidebar"
+import { useCallback, useRef } from "react"
+import Image from "next/image"
+import { AnimatePresence, motion } from "framer-motion"
+import { Globe, Paperclip, Plus, Send } from "lucide-react"
+import { Textarea } from "@/registry/new-york/ui/textarea"
+
+interface UseAutoResizeTextareaProps {
+  minHeight: number
+  maxHeight?: number
+}
+
+function useAutoResizeTextarea({
+  minHeight,
+  maxHeight,
+}: UseAutoResizeTextareaProps) {
+  const textareaRef = useRef<HTMLTextAreaElement>(null)
+
+  const adjustHeight = useCallback(
+    (reset?: boolean) => {
+      const textarea = textareaRef.current
+      if (!textarea) return
+
+      if (reset) {
+        textarea.style.height = `${minHeight}px`
+        return
+      }
+
+      textarea.style.height = `${minHeight}px`
+      const newHeight = Math.max(
+        minHeight,
+        Math.min(textarea.scrollHeight, maxHeight ?? Number.POSITIVE_INFINITY)
+      )
+
+      textarea.style.height = `${newHeight}px`
+    },
+    [minHeight, maxHeight]
+  )
+
+  useEffect(() => {
+    const textarea = textareaRef.current
+    if (textarea) {
+      textarea.style.height = `${minHeight}px`
+    }
+  }, [minHeight])
+
+  useEffect(() => {
+    const handleResize = () => adjustHeight()
+    window.addEventListener("resize", handleResize)
+    return () => window.removeEventListener("resize", handleResize)
+  }, [adjustHeight])
+
+  return { textareaRef, adjustHeight }
+}
+
+const MIN_HEIGHT = 48
+const MAX_HEIGHT = 164
+
+const AnimatedPlaceholder = ({ showSearch, showResearch }: { showSearch: boolean, showResearch: boolean }) => (
+  <AnimatePresence mode="wait">
+    <motion.p
+      key={showSearch ? "search" : "ask"}
+      initial={{ opacity: 0, y: 5 }}
+      animate={{ opacity: 1, y: 0 }}
+      exit={{ opacity: 0, y: -5 }}
+      transition={{ duration: 0.1 }}
+      className="pointer-events-none absolute w-[150px] text-sm text-muted-foreground"
+    >
+      {showSearch ? "Search the web..." : showResearch ? "Show Thinking..." : "Ask Friday..."}
+    </motion.p>
+  </AnimatePresence>
+)
+
+export function AiInput() {
+  const [value, setValue] = useState("")
+  const { textareaRef, adjustHeight } = useAutoResizeTextarea({
+    minHeight: MIN_HEIGHT,
+    maxHeight: MAX_HEIGHT,
+  })
+  const [showSearch, setShowSearch] = useState(false)
+  const [showResearch, setShowReSearch] = useState(false)
+  const [imagePreview, setImagePreview] = useState<string | null>(null)
+  const fileInputRef = useRef<HTMLInputElement>(null)
+
+  const handelClose = (e: any) => {
+    e.preventDefault()
+    e.stopPropagation()
+    if (fileInputRef.current) {
+      fileInputRef.current.value = "" // Reset file input
+    }
+    setImagePreview(null) // Use null instead of empty string
+  }
+
+  const handelChange = (e: any) => {
+    const file = e.target.files ? e.target.files[0] : null
+    if (file) {
+      setImagePreview(URL.createObjectURL(file))
+    }
+  }
+
+  const handleSubmit = () => {
+    setValue("")
+    adjustHeight(true)
+  }
+
+  useEffect(() => {
+    return () => {
+      if (imagePreview) {
+        URL.revokeObjectURL(imagePreview)
+      }
+    }
+  }, [imagePreview])
+  return (
+    <div className="fixed bottom-0 w-full py-4">
+      <div className="relative mx-auto w-full max-w-xl rounded-[22px] p-1">
+        <div className="relative flex flex-col rounded-2xl border bg-primary-foreground">
+          <div
+            className="overflow-y-auto"
+            style={{ maxHeight: `${MAX_HEIGHT}px` }}
+          >
+            <div className="relative">
+              <Textarea
+                id="ai-input-04"
+                value={value}
+                placeholder=""
+                className="w-full resize-none rounded-2xl rounded-b-none border-none px-4 py-3 leading-[1.2] focus-visible:ring-0 "
+                ref={textareaRef}
+                onKeyDown={(e) => {
+                  if (e.key === "Enter" && !e.shiftKey) {
+                    e.preventDefault()
+                    handleSubmit()
+                  }
+                }}
+                onChange={(e) => {
+                  setValue(e.target.value)
+                  adjustHeight()
+                }}
+              />
+              {!value && (
+                <div className="absolute left-4 top-3">
+                  <AnimatedPlaceholder showResearch={showResearch} showSearch={showSearch} />
+                </div>
+              )}
+            </div>
+          </div>
+
+          <div className="h-12 rounded-b-xl ">
+            <div className="absolute bottom-3 left-3 flex items-center">
+              <label
+                className={cn(
+                  "relative cursor-pointer rounded-full p-2",
+                  imagePreview
+                    ? "border text-primary"
+                    : "  "
+                )}
+              >
+                <input
+                  type="file"
+                  ref={fileInputRef}
+                  onChange={handelChange}
+                  className="hidden"
+                />
+                <Paperclip
+                  className={cn(
+                    "h-4 w-4  text-muted-foreground transition-colors hover:text-primary",
+                    imagePreview && "text-primary"
+                  )}
+                />
+                {imagePreview && (
+                  <div className="absolute bottom-10 left-1/2 h-[50px] w-[50px]">
+                    <Image
+                      className="rounded-lg object-cover"
+                      src={imagePreview || "/picture1.jpeg"}
+                      height={500}
+                      width={500}
+                      alt="additional image"
+                    />
+                    <button
+                      onClick={handelClose}
+                      className="shadow-3xl absolute -left-1 -top-1 rotate-45 rounded-lg"
+                    >
+                      <Plus className="h-4 w-4" />
+                    </button>
+                  </div>
+                )}
+              </label>
+              <button
+                type="button"
+                onClick={() => {
+                  setShowSearch(!showSearch)
+                }}
+                className={cn(
+                  "flex h-8 items-center gap-1 rounded-full border px-2 py-0.5 transition-all",
+                  showSearch
+                    ? "border bg-background text-muted-foreground hover:text-primary"
+                    : "border-transparent"
+                )}
+              >
+                <div className="flex h-4 w-4 shrink-0 items-center justify-center">
+                  <motion.div
+                    animate={{
+                      rotate: showSearch ? 180 : 0,
+                      scale: showSearch ? 1.1 : 1,
+                    }}
+                    whileHover={{
+                      rotate: showSearch ? 180 : 15,
+                      scale: 1.1,
+                      transition: {
+                        type: "spring",
+                        stiffness: 300,
+                        damping: 10,
+                      },
+                    }}
+                    transition={{
+                      type: "spring",
+                      stiffness: 260,
+                      damping: 25,
+                    }}
+                  >
+                    <Globe
+                      className={cn(
+                        "h-4 w-4 text-muted-foreground hover:text-primary",
+                        showSearch ? "text-primary" : "text-muted-foreground"
+                      )}
+                    />
+                  </motion.div>
+                </div>
+                <AnimatePresence>
+                  {showSearch && (
+                    <motion.span
+                      initial={{ width: 0, opacity: 0 }}
+                      animate={{
+                        width: "auto",
+                        opacity: 1,
+                      }}
+                      exit={{ width: 0, opacity: 0 }}
+                      transition={{ duration: 0.2 }}
+                      className="shrink-0 overflow-hidden whitespace-nowrap text-[11px] text-primary"
+                    >
+                      Search
+                    </motion.span>
+                  )}
+                </AnimatePresence>
+              </button>
+              <button
+                type="button"
+                onClick={() => {
+                  setShowReSearch(!showResearch)
+                }}
+                className={cn(
+                  "flex h-8 items-center gap-2 rounded-full border px-1.5 py-1 transition-all",
+                  showResearch
+                    ? "border bg-background text-muted-foreground hover:text-primary"
+                    : "border-transparent"
+                )}
+              >
+                <div className="flex h-4 w-4 shrink-0 items-center justify-center">
+                  <motion.div
+                    animate={{
+                      rotate: showResearch ? 180 : 0,
+                      scale: showResearch ? 1.1 : 1,
+                    }}
+                    whileHover={{
+                      rotate: showResearch ? 180 : 15,
+                      scale: 1.1,
+                      transition: {
+                        type: "spring",
+                        stiffness: 300,
+                        damping: 10,
+                      },
+                    }}
+                    transition={{
+                      type: "spring",
+                      stiffness: 260,
+                      damping: 25,
+                    }}
+                  >
+                    <CircleDotDashed
+                      className={cn(
+                        "h-4 w-4 text-muted-foreground hover:text-primary",
+                        showResearch ? "text-primary" : "text-muted-foreground"
+                      )}
+                    />
+                  </motion.div>
+                </div>
+                <AnimatePresence>
+                  {showResearch && (
+                    <motion.span
+                      initial={{ width: 0, opacity: 0 }}
+                      animate={{
+                        width: "auto",
+                        opacity: 1,
+                      }}
+                      exit={{ width: 0, opacity: 0 }}
+                      transition={{ duration: 0.2 }}
+                      className="shrink-0 overflow-hidden whitespace-nowrap text-[11px] text-primary"
+                    >
+                      Research
+                    </motion.span>
+                  )}
+                </AnimatePresence>
+              </button>
+            </div>
+            <div className="absolute bottom-3 right-3">
+              <button
+                type="button"
+                onClick={handleSubmit}
+                className={cn(
+                  "rounded-full p-2 text-muted-foreground transition-colors hover:text-primary",
+                  value
+                    ? " text-primary"
+                    : " text-muted-foreground    "
+                )}
+              >
+                <Send className="h-4 w-4" />
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+
 
 export function CategoryRightSidebar() {
   const id = useId();
@@ -83,14 +409,6 @@ export function CategoryRightSidebar() {
 
   const { categorySidebarState, categorySidebarToggleSidebar } = useCategorySidebar();
   const { subCategorySidebarState, subCategorySidebarToggleSidebar } = useSubCategorySidebar();
-
-  // const handleCategorySidebarToggle = () => {
-  //   categorySidebarToggleSidebar();
-  //   if (subCategorySidebarState === "expanded") {
-  //     subCategorySidebarToggleSidebar();
-  //   }
-  // };
-
   return (
     <CategorySidebar side="right">
       <CategorySidebarHeader>
@@ -411,8 +729,6 @@ export function RightSidebar() {
       </div>
       <CategoryRightSidebar />
       <SubCategoryRightSidebar />
-      {/* <CategorySidebar style={{ zIndex: categorySidebarState === "expanded" ? 20 : 10 }}  />
-      <SubCategorySidebar style={{ zIndex: subCategorySidebarState === "expanded" ? 20 : 10 }} /> */}
     </div>
   )
 }
@@ -436,6 +752,7 @@ export default function Page() {
           </SubCategorySidebarProvider>
         </CategorySidebarProvider>
       </header>
+      <AiInput />
     </main>
   )
 }
