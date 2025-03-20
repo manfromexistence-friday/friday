c98b444e179bd77dfb3256d5040e715875ddb75d
manfromexistence02 <167592146+manfromexistence02@users.noreply.github.com>
2025-03-20 13:25:41 +0000

    feat: update favicon and site manifest for improved branding and consistency

--- frontend/www/app/(app)/page.tsx
+++ frontend/www/app/(app)/page.tsx
@@ -3,13 +3,15 @@
 import * as React from "react"
 import AiInput from '@/components/ai-input'
 import Tags from "@/components/tags"
+import Friday from "@/components/friday/friday"
 
 export default function Home() {
   return (
-    <div className="w-full flex flex-col min-h-screen py-4 gap-4">
+    <div className="w-full flex flex-col min-h-screen py-4 gap-4 items-center justify-center">
       <h1 className="bold text-3xl w-full font-sans text-center">Friday - Your ai friend.</h1>
       <AiInput />
       <Tags />
+      {/* <Friday orbSize={100} shapeSize={90} /> */}
     </div>
   )
 }
