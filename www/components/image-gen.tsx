dde66babcd4d51fde9f62fc76525e13c1c28a082
MD. SIAM BISWAS <167656511+manfromexistence01@users.noreply.github.com>
2025-03-30 16:39:51 +0000

    fix: remove unnecessary whitespace in image generation component

--- www/components/image-gen.tsx
+++ www/components/image-gen.tsx
@@ -140,7 +140,6 @@ export default function ImageGen({ message }: { message: Message }) {
                   </Card>
                 ))
               ) : null}
-
             </div>
           )}
         </div>
