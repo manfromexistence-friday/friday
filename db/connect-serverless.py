95fbd42eec1cf9a101965866eec20596d28821c7
manfromexistence03 <167655168+manfromexistence03@users.noreply.github.com>
2025-04-12 03:58:34 +0000

    feat: refactor connect-serverless script to improve database connection and document insertion

--- db/connect-serverless.py
+++ db/connect-serverless.py
@@ -1,6 +1,25 @@
 from astrapy import DataAPIClient
-client = DataAPIClient("TOKEN")
-database = client.get_database("API_ENDPOINT")
+import uuid
 
-collection = database.get_collection("COLLECTION_NAME")
-collection.count_documents({}, upper_bound=100)  # will print e.g.: 41
+ASTRA_DB_ID = "8202f58c-eaf8-465e-8437-a6c69f2c00f1"
+ASTRA_DB_REGION = "eu-west-1"
+ASTRA_DB_APPLICATION_TOKEN = "AstraCS:CqiNIqJTqzPIKUUhyGLhYdZT:d675d95d187295df16dfe0ba3d26dd3c2f334131fa24bbe59f99fa1406782811"
+ASTRA_DB_KEYSPACE = "authentication"
+COLLECTION_NAME = "test"
+
+client = DataAPIClient(ASTRA_DB_APPLICATION_TOKEN)
+api_endpoint = f"https://{ASTRA_DB_ID}-{ASTRA_DB_REGION}.apps.astra.datastax.com"
+database = client.get_database(api_endpoint)
+collection = database.get_collection(COLLECTION_NAME, namespace=ASTRA_DB_KEYSPACE)
+
+cliff_uuid = str(uuid.uuid4())
+print(f'Sending request to collection: {COLLECTION_NAME}, with uuid: {cliff_uuid}')
+
+document = {
+  "_id": cliff_uuid,
+  "first_name": "Cliff",
+  "last_name": "Wicklow",
+}
+response = collection.insert_one(document)
+
+print(response)
